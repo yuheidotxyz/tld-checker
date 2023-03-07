7273bb96af288055b457019dd80cd70b9083597e
yuheidotxyz <www@yuhei.xyz>
2023-03-07 21:41:10 +0900

    ドメインの種別によってアイコンを変更する機能

---
+++ main.js
@@ -0,0 +1,58 @@
+function create_icon(char = "a", color = "black", back_color = "black") {
+    const canvas = new OffscreenCanvas(128, 128), ctx = canvas.getContext("2d");
+    ctx.fillStyle = back_color;
+    ctx.fillRect(0, 0, 128, 128);
+    ctx.fillStyle = color;
+    ctx.font = "128px sans-serif";
+    ctx.fillText(char, 0, 100);
+    return ctx.getImageData(0, 0, 128, 128);
+}
+function change_icon(tab) {
+    try {
+        const url = new URL(tab.url);
+        const tld_list = {
+            gov: {
+                tlds: [".go.jp", ".lg.jp", ".gov"],
+                char: "g",
+                color: "black",
+                back_color: "green"
+            },
+            edu: {
+                tlds: [".ac.jp"],
+                char: "e",
+                color: "black",
+                back_color: "red"
+            },
+            com: {
+                tlds: [".co.jp"],
+                char: "c",
+                color: "black",
+                back_color: "gold"
+            }
+        }
+        let char, color, back_color;
+        Object.values(tld_list).forEach((value) => {
+            value.tlds.forEach((tld) => {
+                if (url.hostname.endsWith(tld)) {
+                    char = value.char, color = value.color, back_color = value.back_color;
+                }
+            });
+        });
+        chrome.action.setIcon({
+            imageData: create_icon(char, color, back_color)
+        });
+        chrome.action.setTitle({ title: url.origin });
+    }
+    catch (e) {
+        chrome.action.setIcon({
+            imageData: create_icon()
+        });
+        chrome.action.setTitle({ title: "error" });
+    }
+}
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    chrome.tabs.get(activeInfo.tabId, (tab) => { change_icon(tab); });
+});
+chrome.tabs.onUpdated.addListener((tabid, changeInfo, tab) => {
+    change_icon(tab);
+});
